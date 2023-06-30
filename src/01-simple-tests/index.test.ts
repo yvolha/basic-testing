9f3ba6f8156839badcb65acf4995a281e28577bf
Olya Y <f_akora@mail.ru>
2023-07-01 00:59:14 +0300

    refactor: run linter

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -3,58 +3,72 @@ import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    expect(simpleCalculator({
-      a: 1, 
-      b: 2, 
-      action: Action.Add,
-    })).toEqual(3);
+    expect(
+      simpleCalculator({
+        a: 1,
+        b: 2,
+        action: Action.Add,
+      }),
+    ).toEqual(3);
   });
 
   test('should subtract two numbers', () => {
-    expect(simpleCalculator({
-      a: 5, 
-      b: 2, 
-      action: Action.Subtract,
-    })).toEqual(3);
+    expect(
+      simpleCalculator({
+        a: 5,
+        b: 2,
+        action: Action.Subtract,
+      }),
+    ).toEqual(3);
   });
 
   test('should multiply two numbers', () => {
-    expect(simpleCalculator({
-      a: 5, 
-      b: 2, 
-      action: Action.Multiply,
-    })).toEqual(10);
+    expect(
+      simpleCalculator({
+        a: 5,
+        b: 2,
+        action: Action.Multiply,
+      }),
+    ).toEqual(10);
   });
 
   test('should divide two numbers', () => {
-    expect(simpleCalculator({
-      a: 6, 
-      b: 2, 
-      action: Action.Divide,
-    })).toEqual(3);
+    expect(
+      simpleCalculator({
+        a: 6,
+        b: 2,
+        action: Action.Divide,
+      }),
+    ).toEqual(3);
   });
 
   test('should exponentiate two numbers', () => {
-    expect(simpleCalculator({
-      a: 2, 
-      b: 2, 
-      action: Action.Exponentiate,
-    })).toEqual(4);
+    expect(
+      simpleCalculator({
+        a: 2,
+        b: 2,
+        action: Action.Exponentiate,
+      }),
+    ).toEqual(4);
   });
 
   test('should return null for invalid action', () => {
-    expect(simpleCalculator({
-      a: 2,
-      b: 2, 
-      action: 'cook',
-    })).toEqual(null);
+    expect(
+      simpleCalculator({
+        a: 2,
+        b: 2,
+        action: 'cook',
+      }),
+    ).toEqual(null);
   });
 
   test('should return null for invalid arguments', () => {
-    expect(simpleCalculator({
-      a: 'cat', 
-      b: 'dog', 
-      action: Action.Multiply,
-    })).toEqual(null);
+    expect(
+      simpleCalculator({
+        a: 'cat',
+        b: 'dog',
+        action: Action.Multiply,
+      }),
+    ).toEqual(null);
   });
 });
--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,22 +1,27 @@
 // Uncomment the code below and write your tests
-import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 5, b: 2, action: Action.Subtract, expected: 3 },
-    { a: 3, b: 2, action: Action.Multiply, expected: 6 },
-    { a: 10, b: 2, action: Action.Divide, expected: 5 },
-    { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
-    { a: 10, b: 2, action: 'sleep', expected: null },
-    { a: {woof: 'meow'}, b: true, action: Action.Subtract, expected: null },
-]; 
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 5, b: 2, action: Action.Subtract, expected: 3 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 10, b: 2, action: 'sleep', expected: null },
+  { a: { woof: 'meow' }, b: true, action: Action.Subtract, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  test.each(testCases)('perform operations with ($a, $b)', ({a, b, action, expected}) => {
-    expect(simpleCalculator({
-      a: a,
-      b: b,
-      action: action,
-    })).toBe(expected);
-  });
+  test.each(testCases)(
+    'perform operations with ($a, $b)',
+    ({ a, b, action, expected }) => {
+      expect(
+        simpleCalculator({
+          a: a,
+          b: b,
+          action: action,
+        }),
+      ).toBe(expected);
+    },
+  );
 });
